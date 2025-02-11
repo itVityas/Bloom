ac646c5f24023f487e08c8b3d2306bad7d4b4a6d
AntonKralin <antonkralin91@gmail.com>
2025-02-11 11:26:06 +0300

    change sgp

--- apps/sgp/views/consignment.py
+++ apps/sgp/views/consignment.py
@@ -14,15 +14,15 @@ from apps.sgp.serializers.consignment import ConsignmentSerializer
         description='''
         isAdmin
     document_number = CharField(max_length=20)
-    doc_date = DateField()
+    doc_date = Integer (timestamp)
     recipient = CharField(max_length=250)
     recipient_code = CharField(max_length=7)
-    quantity = IntegerField()
-    article = CharField(max_length=25)
-    invoice_number = CharField(max_length=20)
-    invoice_series = CharField(max_length=4)
+    quantity = FloatField()
+    article = CharField(max_length=25, required=False)
+    invoice_number = CharField(max_length=20, required=False)
+    invoice_series = CharField(max_length=4, required=False)
     unp = CharField(max_length=20)
-    gtin = CharField(max_length=14)
+    gtin = CharField(max_length=14, required=False)
         '''
     ),
 )
