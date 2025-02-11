30612882ac11d2fcc240f3f57d985089cce0a1a7
AntonKralin <antonkralin91@gmail.com>
2025-02-11 15:19:36 +0300

    add null field

--- apps/sgp/serializers/consignment.py
+++ apps/sgp/serializers/consignment.py
@@ -4,8 +4,10 @@ from rest_framework import serializers
 class ConsignmentSerializer(serializers.Serializer):
     document_number = serializers.CharField(max_length=20)
     doc_date = serializers.IntegerField()
-    recipient = serializers.CharField(max_length=250)
-    recipient_code = serializers.CharField(max_length=7)
+    recipient = serializers.CharField(max_length=250, required=False,
+                                      allow_blank=True, allow_null=True)
+    recipient_code = serializers.CharField(max_length=7, required=False,
+                                           allow_blank=True, allow_null=True)
     quantity = serializers.FloatField()
     article = serializers.CharField(max_length=25, required=False,
                                     allow_blank=True, allow_null=True)
