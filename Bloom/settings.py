5bc1931a242060c438e9a6fe20e1e57a74983552
Vlad <foile.dm@gmail.com>
2025-02-17 09:29:34 +0300

    Change JWT token lifetime

--- Bloom/settings.py
+++ Bloom/settings.py
@@ -149,7 +149,7 @@ CORS_ALLOW_HEADERS = [
 
 # settings for autorizations
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=8),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
