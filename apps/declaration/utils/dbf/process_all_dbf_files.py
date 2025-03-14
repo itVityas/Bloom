dbaab4bb06f3dea9270903c15fef8893f918ebac
Vlad <foile.dm@gmail.com>
2025-03-14 15:51:32 +0300

    Add container param to process func

--- apps/declaration/utils/dbf/process_all_dbf_files.py
+++ apps/declaration/utils/dbf/process_all_dbf_files.py
@@ -6,10 +6,10 @@ from .decl import process_decl_dbf_file
 from .tovar import process_tovar_dbf_file
 # from .g18 import process_g18_dbf_file
 # from .g40 import process_g40_dbf_file
-# from .g44 import process_g44_dbf_file
+from .g44 import process_g44_dbf_file
 # from .g47 import process_g47_dbf_file
 # from .g48 import process_g48_dbf_file
-# from .g313 import process_g313_dbf_file
+from .g313 import process_g313_dbf_file
 # from .gb import process_gb_dbf_file
 
 
@@ -26,22 +26,21 @@ PROCESSING_FUNCTIONS = {
 }
 
 
-def process_all_dbf_files(zip_file_path):
+def process_all_dbf_files(zip_file_path, container=None):
     """
     Process all DBF files contained in the provided zip archive.
 
     The zip archive must contain the following files (with exact names):
     DECL.DBF, G18.DBF, G40.DBF, G44.DBF, G47.DBF, G48.DBF, G313, GB.DBF, TOVAR.DBF.
 
-    If any of these required files is missing, an Exception is raised with a message
-    listing the missing file names.
-
     For each file present in the archive, the corresponding processing function is called
-    with the path to the extracted temporary file. All temporary files (including the original zip file)
-    are removed after processing.
+    with the path to the extracted temporary file. For the file 'DECL.DBF', the provided container
+    instance is also passed to associate the declarations with that container.
+    All temporary files (including the original zip file) are removed after processing.
 
     Args:
         zip_file_path (str): The file system path to the uploaded zip archive.
+        container (Optional[Container]): An optional container instance to associate with declarations.
 
     Raises:
         Exception: If one or more required files are missing or if any exception occurs during extraction or processing.
@@ -58,7 +57,10 @@ def process_all_dbf_files(zip_file_path):
                 tmp_file.write(zip_ref.read(file_name))
                 tmp_file_path = tmp_file.name
             try:
-                process_func(tmp_file_path)
+                if file_name == 'DECL.DBF':
+                    process_func(tmp_file_path, container=container)
+                else:
+                    process_func(tmp_file_path)
             finally:
                 if os.path.exists(tmp_file_path):
                     os.remove(tmp_file_path)
