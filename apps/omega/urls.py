f284425f73eed3105f54176581d492cdc90e2675
Vlad <foile.dm@gmail.com>
2025-04-15 10:04:33 +0300

    Update vz_nab endpoint url, add vz_nab url

--- apps/omega/urls.py
+++ apps/omega/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from apps.omega.views.vz_nab import VzNabListAPIView
 from apps.omega.views.vz_norm import VzNormListAPIView
 
+
 urlpatterns = [
-    path('omega/', VzNormListAPIView.as_view(), name='vznorms-list'),
+    path('omega/vz_norm/', VzNormListAPIView.as_view(), name='vznorms-list'),
+    path('omega/vz_nab/', VzNabListAPIView.as_view(), name='vznab-list'),
+
 ]
