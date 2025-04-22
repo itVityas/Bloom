39043d3a58d09ef0f1d2bf25d536c61c224d6237
AntonKralin <antonkralin91@gmail.com>
2025-04-22 09:08:51 +0300

    add model name by product code

--- apps/shtrih/urls.py
+++ apps/shtrih/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.shtrih.views.color import ColorsListView
 from apps.shtrih.views.module import ModulesListView
 from apps.shtrih.views.production_code import ProductionCodeListView
-from apps.shtrih.views.model_name import ModelNameListView
+from apps.shtrih.views.model_name import ModelNameListView, ModelNameByProductCodeListView
 from apps.shtrih.views.model import ModelListView
 from apps.shtrih.views.product import ProductListView
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('strih/modules', ModulesListView.as_view()),
     path('strih/production_code', ProductionCodeListView.as_view()),
     path('strih/model_name', ModelNameListView.as_view()),
+    path('strih/model_name_by_production_id', ModelNameByProductCodeListView.as_view()),
     path('strih/models', ModelListView.as_view()),
     path('strih/products', ProductListView.as_view()),
 ]
--- apps/shtrih/views/model_name.py
+++ apps/shtrih/views/model_name.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.response import Response
 
 from apps.shtrih.models import ModelNames
 from apps.shtrih.serializers.model_name import ModelNamesSerializer
@@ -23,3 +25,39 @@ class ModelNameListView(ListAPIView):
     pagination_class = StandartResultPaginator
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('id', 'name', 'short_name')
+
+
+@extend_schema(tags=['Shtrih'])
+@extend_schema_view(
+    get=extend_schema(
+        summary='Get list Model by product_code',
+        description="description='Permission: admin, strih",
+        parameters=[
+            OpenApiParameter(
+                name='production_code_id',
+                location=OpenApiParameter.QUERY,
+                description='production_code.id',
+                required=True,
+                type=int,
+            ),
+        ],
+    )
+)
+class ModelNameByProductCodeListView(ListAPIView):
+    queryset = ModelNames.objects.all()
+    serializer_class = ModelNamesSerializer
+    permission_classes = (IsAuthenticated, StrihPermission)
+    pagination_class = StandartResultPaginator
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('id', 'name', 'short_name')
+
+    def get(self, request):
+        production_code_id = request.query_params.get('production_code_id', None)
+        if not production_code_id:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={'error': 'product_code not fount'}
+            )
+        queryset = ModelNames.objects.filter(models__production_code=production_code_id)
+        serializer = ModelNamesSerializer(queryset, many=True)
+        return Response(serializer.data)
