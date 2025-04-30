a6cdcda5727139a1a22a0ab58184026a8abb30f9
Vlad <foile.dm@gmail.com>
2025-04-30 11:17:30 +0300

    Delete not used endpoint for omega

--- apps/omega/serializers/vz_nab.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.omega.models import VzNab
-
-
-class VzNabSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VzNab
-        fields = '__all__'
--- apps/omega/urls.py
+++ apps/omega/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 
-from apps.omega.views.vz_nab import VzNabListAPIView
-from apps.omega.views.vz_norm import VzNormListAPIView
 from apps.omega.views.clear_items import ClearItemsView
 
 
 urlpatterns = [
-    path('omega/vz_norm/', VzNormListAPIView.as_view(), name='vznorms-list'),
-    path('omega/vz_nab/', VzNabListAPIView.as_view(), name='vznab-list'),
     path("omega/clear_items/", ClearItemsView.as_view(), name="clear-items"),
 
 ]
