4cc28c05c49a3080cf853d40c0bcc49d7130baa7
Vlad <foile.dm@gmail.com>
2025-05-19 09:25:28 +0300

    refactor: remove old ClearItems functionality and related endpoints

--- apps/omega/urls.py
+++ apps/omega/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from apps.omega.views.clear_items import ClearItemsView
 
 
 urlpatterns = [
-    path("omega/clear_items/", ClearItemsView.as_view(), name="clear-items"),
 
 ]
