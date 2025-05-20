2b7a0273b6e576f2ef06069e84d31861fbe7ae51
AntonKralin <antonkralin91@gmail.com>
2025-05-20 10:11:42 +0300

    add full price and quantity

--- apps/sez/serializers/inner_ttn.py
+++ apps/sez/serializers/inner_ttn.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from apps.sez.models import InnerTTN, InnerTTNItems
 from apps.sez.serializers.inner_ttn_item import InnerTTNItemsSerializer
+from django.db.models import Sum
 
 
 class InnerTTNPostSerializer(serializers.ModelSerializer):
@@ -12,6 +13,8 @@ class InnerTTNPostSerializer(serializers.ModelSerializer):
 
 class InnerTTNSerializer(serializers.ModelSerializer):
     items = serializers.SerializerMethodField()
+    full_price = serializers.SerializerMethodField()
+    full_quantity = serializers.SerializerMethodField()
 
     class Meta:
         model = InnerTTN
@@ -20,3 +23,15 @@ class InnerTTNSerializer(serializers.ModelSerializer):
     def get_items(self, obj) -> dict:
         items = InnerTTNItems.objects.filter(inner_ttn=obj)
         return InnerTTNItemsSerializer(items, many=True).data
+
+    def get_full_price(self, obj) -> float:
+        price = InnerTTNItems.objects.filter(inner_ttn=obj).aggregate(
+            full_price=Sum("price_pcs")
+        )
+        return price.get('full_price', 0)
+
+    def get_full_quantity(self, obj) -> int:
+        quantity = InnerTTNItems.objects.filter(inner_ttn=obj).aggregate(
+            full_quantity=Sum("quantity")
+        )
+        return quantity.get('full_quantity', 0)
