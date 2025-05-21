ee1052249779c967e911750d3a469d46b14c9796
AntonKralin <antonkralin91@gmail.com>
2025-05-21 08:37:54 +0300

    add auth app

--- Bloom/settings.py
+++ Bloom/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'rest_framework_simplejwt',
     'drf_spectacular',
