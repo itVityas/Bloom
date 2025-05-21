556747a83f6d73ea2a69b94ebc3369e011e4a7ee
Vlad <foile.dm@gmail.com>
2025-05-21 16:25:43 +0300

    refactor: localize API response

--- apps/declaration/utils/dbf/duplicate_exception.py
+++ apps/declaration/utils/dbf/duplicate_exception.py
@@ -1,4 +1,4 @@
 class DuplicateDeclarationException(Exception):
     def __init__(self, duplicate_ids):
         self.duplicate_ids = duplicate_ids
-        super().__init__(f"Declarations with declaration_ids {duplicate_ids} already exist.")
+        super().__init__(f"Декларации с declaration_ids {duplicate_ids} уже существуют.")
