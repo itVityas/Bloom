3a841c7477644140017e3296dec564ee2d45fa21
AntonKralin <antonkralin91@gmail.com>
2025-05-29 08:20:02 +0300

    add 1c weigth

--- apps/onec/urls.py
+++ apps/onec/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from apps.onec.views.weight import Weight1cView
+
 urlpatterns = [
-    
+    path('1c/weight/', Weight1cView.as_view()),
 ]
---
+++ apps/onec/views/weight.py
@@ -0,0 +1,48 @@
+import requests
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from drf_spectacular.utils import (
+    extend_schema, extend_schema_view, OpenApiParameter, OpenApiResponse)
+
+from django.conf import settings
+
+
+@extend_schema(tags=['1c'])
+@extend_schema_view(
+    get=extend_schema(
+        summary='get weight from 1c',
+        description='Permission: auth user',
+        parameters=[
+            OpenApiParameter(
+                name='model_name',
+                description='model name',
+                required=True,
+                type=str
+            ),
+        ],
+        responses={
+            200: OpenApiResponse(description="json from 1C"),
+            400: OpenApiResponse(description="Missing required parameters"),
+        }
+    ),
+)
+class Weight1cView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        model_name = request.query_params.get('model_name', None)
+        if not model_name:
+            return Response({'error': 'Missing required parameters'}, status=status.HTTP_400_BAD_REQUEST)
+
+        url = f'http://192.168.2.2/OLYA/hs/bloom/weight?model={model_name}'
+        response = requests.get(
+            url,
+            auth=(settings.API_USERNAME, settings.API_PASSWORD),
+            timeout=30
+        )
+        if response.status_code != 200:
+            return Response({'error': 'Bad response from 1C'})
+        return Response(response.json(), status=status.HTTP_200_OK)
