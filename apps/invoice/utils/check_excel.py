c3b491340da7f47d3dfcb09b4092308131c63f2d
AntonKralin <antonkralin91@gmail.com>
2025-06-13 15:39:24 +0300

    change check found including

--- apps/invoice/utils/check_excel.py
+++ apps/invoice/utils/check_excel.py
@@ -3,15 +3,17 @@ from openpyxl import load_workbook
 
 def find_sheet(invoice_number, container_name, file) -> str:
     try:
+        invoice_number = invoice_number.lower()
+        container_name = invoice_number.lower()
         wb = load_workbook(file)
         for sheet in wb.worksheets:
             container_check = False
             number_check = False
             for row in sheet.iter_rows(values_only=True, max_row=30):
                 for cell in row:
-                    if cell == container_name:
+                    if str(cell).lower().find(container_name) != -1:
                         container_check = True
-                    if cell == invoice_number:
+                    if str(cell).lower().find(invoice_number) != -1:
                         number_check = True
                     if container_check and number_check:
                         return sheet.title
