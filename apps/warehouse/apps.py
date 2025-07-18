a7cf5e225f51ac9eb34154809a4fca9872599661
Anton Kralin <kralin.dart@gmail.com>
2025-07-18 09:40:26 +0300

    add warehouse history signal

--- apps/warehouse/apps.py
+++ apps/warehouse/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WarehouseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.warehouse'
+
+    def ready(self):
+        from apps.warehouse.signals.warehouse_product import WarehouseProductSignal
