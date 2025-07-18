15555af28f5f9405cd9d3ba3384aa302533769df
Anton Kralin <kralin.dart@gmail.com>
2025-07-18 14:46:57 +0300

    check and get or create warehouse ttn

--- apps/warehouse/serializers/warehouse_products.py
+++ apps/warehouse/serializers/warehouse_products.py
@@ -1,6 +1,7 @@
+from django.utils import timezone
 from rest_framework import serializers
 
-from apps.warehouse.models import WarehouseProduct
+from apps.warehouse.models import WarehouseProduct, WarehouseTTN
 from apps.warehouse.serializers.warehouse import WarehouseSerializer
 from apps.warehouse.serializers.warehouse_action import (
     WarehouseActionGetSerializer)
@@ -37,6 +38,7 @@ class WarehouseProductGetSerializer(serializers.ModelSerializer):
 
 
 class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
+    warehouse_ttn = serializers.CharField()
     barcode = serializers.CharField(write_only=True)
 
     class Meta:
@@ -55,6 +57,16 @@ class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
             'user': {'read_only': True}
         }
 
+    def validate(self, attrs):
+        if attrs.get('warehouse_ttn', None):
+            if not WarehouseTTN.objects.filter(
+                    ttn_number=attrs['warehouse_ttn']).exists():
+                WarehouseTTN.objects.create(
+                    ttn_number=attrs['warehouse_ttn'],
+                    is_close=False,
+                    date=timezone.now())
+        return super().validate(attrs)
+
     def validate_barcode(self, value):
         if not Products.objects.filter(barcode=value).exists():
             raise serializers.ValidationError("Product with this barcode does not exist")
@@ -68,6 +80,8 @@ class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
             validated_data['quantity'] = product.quantity
 
         validated_data['user'] = self.context['request'].user
+        validated_data['warehouse_ttn'] = WarehouseTTN.objects.get(
+            ttn_number=validated_data['warehouse_ttn'])
 
         return WarehouseProduct.objects.create(
             product=product,
