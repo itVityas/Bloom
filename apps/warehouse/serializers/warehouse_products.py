f8c0dc3596b484009fb43d585f2183dde14bc69b
Anton Kralin <kralin.dart@gmail.com>
2025-07-24 08:18:50 +0300

    add ttn fields to warehouse product by barcode

--- apps/warehouse/serializers/warehouse_products.py
+++ apps/warehouse/serializers/warehouse_products.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
-from apps.warehouse.models import WarehouseProduct
+from apps.warehouse.models import (
+    WarehouseProduct,
+    WarehouseDo,
+    WarehouseTTN
+)
 from apps.shtrih.serializers.products import ProductGetSerializer
 from apps.shtrih.models import Products, Protocols
 from apps.warehouse.exceptions.barcode import (
@@ -30,6 +34,10 @@ class WarehouseProductGetSerializer(serializers.ModelSerializer):
 class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
     barcode = serializers.CharField(write_only=True)
     check_packaging = serializers.BooleanField(write_only=True, default=False)
+    number = serializers.CharField(write_only=True, required=True)
+    date = serializers.DateField(write_only=True, required=True)
+    warehouse_id = serializers.IntegerField(write_only=True, required=True)
+    warehouse_action_id = serializers.IntegerField(write_only=True, required=True)
 
     class Meta:
         model = WarehouseProduct
@@ -38,10 +46,13 @@ class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
             'quantity',
             'onec_ttn',
             'check_packaging',
+            'number',
+            'date',
+            'warehouse_id',
+            'warehouse_action_id',
         ]
         extra_kwargs = {
             'product': {'read_only': True},
-            'user': {'read_only': True}
         }
 
     def validate_barcode(self, value):
@@ -53,6 +64,24 @@ class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
         barcode = validated_data.pop('barcode')
         product = Products.objects.get(barcode=barcode)
         check_packaging = validated_data.pop('check_packaging', False)
+        number = validated_data.pop('number', None)
+        date = validated_data.pop('date', None)
+        warehouse_id = validated_data.pop('warehouse_id', None)
+        warehouse_action_id = validated_data.pop('warehouse_action_id', None)
+        user = self.context['request'].user
+
+        if not number or not date or not warehouse_id or not warehouse_action_id:
+            raise serializers.ValidationError(
+                'Не указаны обязательные поля: number, date, warehouse_id, warehouse_action_id')
+        warehouse_ttn = WarehouseTTN.objects.filter(ttn_number=number).first()
+        if not warehouse_ttn:
+            warehouse_ttn = WarehouseTTN.objects.create(
+                ttn_number=number,
+                date=date,
+                warehouse_id=warehouse_id,
+                warehouse_action_id=warehouse_action_id,
+                user=user
+            )
 
         if product:
             if check_packaging:
@@ -68,9 +97,13 @@ class WarehouseProductBarcodeSerializer(serializers.ModelSerializer):
         if not validated_data.get('quantity', None):
             validated_data['quantity'] = product.quantity
 
-        validated_data['user'] = self.context['request'].user
-
-        return WarehouseProduct.objects.create(
+        warehouse_product = WarehouseProduct.objects.create(
             product=product,
             **validated_data
         )
+        WarehouseDo.objects.create(
+            warehouse_product=warehouse_product,
+            warehouse_ttn=warehouse_ttn,
+            user=user
+        )
+        return warehouse_product
