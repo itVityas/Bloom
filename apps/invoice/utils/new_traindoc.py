172830d6240b7e952c8f98986e6770a0bec8f401
Anton Kralin <kralin.dart@gmail.com>
2025-09-04 14:27:28 +0300

    change traindoc income parser

--- apps/invoice/utils/new_traindoc.py
+++ apps/invoice/utils/new_traindoc.py
@@ -17,18 +17,21 @@ def create_invoice(traindoc) -> None:
         date = None
         container = None
         check_container = False
+        row_count = 0
         for row in sheet.iter_rows(values_only=True, max_row=30):
             for idx, cell in enumerate(row):
                 cell_value = str(cell)
 
-                if cell_value == '№:':
+                if cell_value == '№:' or cell_value == 'Specification №':
                     number = row[idx + 1]
-
                 elif any(keyword in cell_value for keyword in ['Date / Дата:', 'Date', 'Дата']):
                     date = row[idx + 1]
+                    if date == '':
+                        date = sheet.cell(row=row_count, column=idx).value
                 elif cell_value.lower() in list_containers:
                     container = containers.filter(name=cell_value).first()
                     check_container = True
+            row_count += 1
 
         if check_container and number and date:
             if not InvoiceContainer.objects.filter(number=number, date=date, container=container).exists():
