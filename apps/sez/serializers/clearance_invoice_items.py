2e13835c3fd3160656fbdcca9257facf66662853
Anton Kralin <kralin.dart@gmail.com>
2025-11-06 08:55:34 +0300

    change calc real_amount

--- apps/sez/serializers/clearance_invoice_items.py
+++ apps/sez/serializers/clearance_invoice_items.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
-from django.db.models import Sum, IntegerField, OuterRef, F, Subquery
-from django.db.models.functions import Coalesce
 
-from apps.sez.models import ClearanceInvoiceItems, ClearedItem
+from apps.sez.models import ClearanceInvoiceItems
 from apps.shtrih.serializers.model_name import ModelNamesSerializer
-from apps.shtrih.models import Models, Products
-from apps.declaration.models import DeclaredItem
+from apps.shtrih.models import Models, Products, ProductTransitions
+from apps.declaration.models import Declaration
 
 
 class ClearanceInvoiceItemsSerializer(serializers.ModelSerializer):
@@ -68,28 +66,26 @@ class ClearanceInvoiceItemsFullSerializer(serializers.ModelSerializer):
         return None
 
     def get_real_amount(self, obj) -> float:
-        declaration_item = obj.declared_item
-        if not declaration_item:
-            try:
-                count = Products.objects.filter(
-                    cleared__isnull=True,
-                    model__name__id=obj.model_name_id.id
-                ).values('barcode').distinct().count()
-                return count
-            except Exception:
-                return 0
+        declaration = obj.declared_item
+        if declaration:
+            return declaration.available_quantity
 
-        cleared_items_subquery = ClearedItem.objects.filter(
-            declared_item_id=OuterRef('id')
-        ).values('declared_item_id').annotate(
-            total_cleared=Sum('quantity', output_field=IntegerField())
-        ).values('total_cleared').order_by()
-
-        real_amount = DeclaredItem.objects.filter(id=declaration_item.id).annotate(
-            real_amount=F('quantity') - Coalesce(Subquery(cleared_items_subquery), 0)
-        ).filter(
-            real_amount__gt=0
-        ).values(
-            'real_amount',
-        ).first()
-        return real_amount['real_amount'] if real_amount else 0
+        order_list = obj.clearance_invoice.order.values_list('id', flat=True)
+        is_gifted = obj.clearance_invoice.is_gifted
+        process_transitions_list = ProductTransitions.objects.all().values_list('old_product')
+        products = Products.objects.filter(model__name__id=obj.model_name_id.id, cleared__isnull=True)
+        if order_list:
+            # get list of decl in order: [('07260/52003398',), ('07260/52001406',),
+            # ('07260/52001405',), ('07260/52001449',), ('07260/52001402',)]
+            declaration_numbers = Declaration.objects.filter(
+                container__order__id__in=order_list, is_use=True).values_list('declaration_number')
+            products = products.filter(consignment__declaration_number__in=declaration_numbers)
+        if is_gifted:
+            declaration_numbers = Declaration.objects.filter(gifted=True, is_use=True).values_list('declaration_number')
+            products = products.filter(consignment__declaration_number__in=declaration_numbers)
+        else:
+            declaration_numbers = Declaration.objects.filter(
+                gifted=False).values_list('declaration_number')
+            products = products.filter(consignment__declaration_number__in=declaration_numbers)
+        products = products.exclude(pk__in=process_transitions_list)
+        return products.count()
