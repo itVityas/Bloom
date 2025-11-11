f935ef3029b35ce3a86e029894efd75e86fef6d0
Anton Kralin <kralin.dart@gmail.com>
2025-11-11 09:24:29 +0300

    change barcode xmls add 2 field date and count

--- apps/sez/views/export_products.py
+++ apps/sez/views/export_products.py
@@ -1,5 +1,6 @@
 # apps/sez/views/export_products.py
 from io import BytesIO
+import calendar
 
 import openpyxl
 from openpyxl.styles import Alignment
@@ -40,7 +41,7 @@ class ClearanceInvoiceProductsExportView(APIView):
             Products.objects
             .filter(cleared_id=invoice_id)
             .select_related('model__name')
-            .order_by('id')
+            .order_by('barcode')
         )
 
         # Create workbook and sheet
@@ -49,22 +50,48 @@ class ClearanceInvoiceProductsExportView(APIView):
         ws.title = f"Invoice_{invoice_id}_Products"
 
         # Write header
-        headers = ["Product ID", "Model Short Name", "Barcode"]
+        headers = ["Number", "Product ID", "Model Short Name", "Barcode", "Date"]
         ws.append(headers)
 
         # Write data rows
+        model_name = ''
+        prev_module = ''
+        count = 0
         for product in products_qs:
             product_id = product.id
             model_short = product.model.name.short_name if product.model and product.model.name else ''
             barcode = product.barcode or ''
-            ws.append([product_id, model_short, barcode])
+            if model_name != product.model.name:
+                ws.append([])
+                model_name = product.model.name
+                count = 0
+            date = ''
+            if barcode:
+                try:
+                    module = barcode[11:12]
+                    mm = int(barcode[7:9])
+                    yy = int('20' + barcode[9:11])
+                    _, num_days = calendar.monthrange(yy, mm)
+                    date = f"{yy}.{mm:02d}.{num_days}"
+                except ValueError:
+                    pass
+
+            if prev_module != module:
+                ws.append([])
+                prev_module = module
+                count = 0
+
+            count += 1
+            ws.append([count, product_id, model_short, barcode, date])
 
         # Adjust column widths and alignment
         # Set wider columns
         column_widths = {
-            1: 15,  # Product ID
-            2: 20,  # Model Short Name
-            3: 30,  # Barcode
+            1: 5,   # count
+            2: 15,  # Product ID
+            3: 20,  # Model Short Name
+            4: 30,  # Barcode
+            5: 30,  # Date
         }
         for col_idx, width in column_widths.items():
             col_letter = get_column_letter(col_idx)
