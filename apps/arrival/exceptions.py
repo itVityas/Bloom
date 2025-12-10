fe16466477318e72fb4bf3989e8ce6d23dd322b3
Anton Kralin <kralin.dart@gmail.com>
2025-12-10 10:55:00 +0300

    change text issue

--- apps/arrival/exceptions.py
+++ apps/arrival/exceptions.py
@@ -5,7 +5,7 @@ class DuplicateContainerException(ValidationError):
     value = 'Контейнер с таким именем уже существует'
 
     def __init__(self, container_name):
-        self.value = f"Контейнер {container_name} уже существует"
+        self.value = f"Контейнер {container_name} в этом заказе уже существует"
         super().__init__(self.value)
 
     def __str__(self):
