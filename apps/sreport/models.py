2c970703e2a7d693efa25361201b1eaeedbfb164
Anton Kralin <kralin.dart@gmail.com>
2025-12-24 15:03:03 +0300

    change report storage view

--- apps/sreport/models.py
+++ apps/sreport/models.py
@@ -5,12 +5,11 @@ from django.db import models
 class ReportStorage(models.Model):
     """
 CREATE VIEW report_storage AS
-SELECT name.id as model_name_id, name.name as name,
-    COUNT(CASE WHEN pr.cleared is null THEN 1 END) as uncleared,
-    COUNT(CASE WHEN pr.cleared is not null AND invoice.recipient_id != 4 THEN 1 END) as cleared,
-    COUNT(CASE WHEN pt.new_product_id is not null THEN 1 END) as simple,
-    COUNT(CASE WHEN invoice.recipient_id=4 THEN 1 END) as compensation,
-    wt.warehouse_id as warehouse_id
+SELECT name.id as model_name_id, name.name as name, COUNT(CASE WHEN pr.cleared is null THEN 1 END) as uncleared,
+	 COUNT(CASE WHEN pr.cleared is not null AND invoice.recipient_id != 4 THEN 1 END) as cleared,
+	 COUNT(CASE WHEN pt.new_product_id is not null THEN 1 END) as simple,
+	 COUNT(CASE WHEN invoice.recipient_id=4 THEN 1 END) as compensation,
+	 wt.warehouse_id as warehouse_id
 FROM products as pr
 JOIN models as mod ON pr.model_id = mod.id
 JOIN model_names as name ON mod.name_id = name.id
@@ -20,7 +19,7 @@ JOIN warehouse_warehousedo wd ON wd.warehouse_product_id = wp.id
 JOIN warehouse_warehousettn wt ON wt.ttn_number = wd.warehouse_ttn_id
 JOIN protocols as prot ON prot.product_id = pr.id
 JOIN invoices as invoice ON invoice.id = prot.invoice_id
-where  (pr.state <> 1)
+where  (pr.state <> 1 and wt.warehouse_id <> 12)
 GROUP BY name.id, name.name, wt.warehouse_id
     """
     model_name_id = models.IntegerField(db_column='model_name_id', primary_key=True)
