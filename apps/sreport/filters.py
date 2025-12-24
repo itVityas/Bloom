7540f11283cebc209b727cb34823061a4acff0a6
Anton Kralin <kralin.dart@gmail.com>
2025-12-24 13:52:39 +0300

    add warehouse id

--- apps/sreport/filters.py
+++ apps/sreport/filters.py
@@ -1,4 +1,5 @@
 import django_filters as filters
+from django.db.models import Sum
 
 from apps.sreport.models import ReportStorage
 from apps.shtrih.models import ModelNames
@@ -10,6 +11,7 @@ class ReportStorageFilter(filters.FilterSet):
     end_name = filters.CharFilter(field_name='name', lookup_expr='iendswith')
     cont_name = filters.CharFilter(field_name='name', lookup_expr='icontains')
     production_code = filters.NumberFilter(method='filter_production_code')
+    warehouse_id = filters.NumberFilter(method='filter_warehouse_id')
 
     ordering = filters.OrderingFilter(
         fields=(
@@ -29,9 +31,22 @@ class ReportStorageFilter(filters.FilterSet):
             'end_name',
             'cont_name',
             'production_code',
+            'warehouse_id',
         ]
 
     def filter_production_code(self, queryset, name, value):
         model_names = ModelNames.objects.distinct().filter(
             models__production_code__code=value).values_list('id', flat=True)
         return queryset.filter(model_name_id__in=model_names)
+
+    def filter_warehouse_id(self, queryset, name, value):
+        if not value:
+            return queryset.values(
+                'model_name_id', 'name'
+            ).annotate(
+                total_uncleared=Sum('uncleared'),
+                total_cleared=Sum('cleared'),
+                total_simple=Sum('simple'),
+                total_compensation=Sum('compensation')
+            )
+        return queryset.filter(warehouse_id=value)
--- apps/sreport/models.py
+++ apps/sreport/models.py
@@ -5,19 +5,23 @@ from django.db import models
 class ReportStorage(models.Model):
     """
 CREATE VIEW report_storage AS
-SELECT name.id as model_name_id, name.name as name, COUNT(CASE WHEN pr.cleared is null THEN 1 END) as uncleared,
+SELECT name.id as model_name_id, name.name as name,
+    COUNT(CASE WHEN pr.cleared is null THEN 1 END) as uncleared,
     COUNT(CASE WHEN pr.cleared is not null AND invoice.recipient_id != 4 THEN 1 END) as cleared,
     COUNT(CASE WHEN pt.new_product_id is not null THEN 1 END) as simple,
-    COUNT(CASE WHEN invoice.recipient_id=4 THEN 1 END) as compensation
+    COUNT(CASE WHEN invoice.recipient_id=4 THEN 1 END) as compensation,
+    wt.warehouse_id as warehouse_id
 FROM products as pr
 JOIN models as mod ON pr.model_id = mod.id
 JOIN model_names as name ON mod.name_id = name.id
 LEFT JOIN product_transitions as pt ON pr.id = pt.new_product_id
 JOIN warehouse_warehouseproduct as wp ON wp.product_id = pr.id
+JOIN warehouse_warehousedo wd ON wd.warehouse_product_id = wp.id
+JOIN warehouse_warehousettn wt ON wt.ttn_number = wd.warehouse_ttn_id
 JOIN protocols as prot ON prot.product_id = pr.id
 JOIN invoices as invoice ON invoice.id = prot.invoice_id
-where pr != 1
-GROUP BY name.id, name.name
+where  (pr.state <> 1)
+GROUP BY name.id, name.name, wt.warehouse_id
     """
     model_name_id = models.IntegerField(db_column='model_name_id', primary_key=True)
     name = models.CharField(max_length=255)
@@ -25,6 +29,7 @@ GROUP BY name.id, name.name
     cleared = models.IntegerField()
     simple = models.IntegerField()
     compensation = models.IntegerField()
+    warehouse_id = models.IntegerField(db_column='warehouse_id')
 
     class Meta:
         managed = False
