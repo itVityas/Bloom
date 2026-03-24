8c1f70fec2960d389f805a8c4baa7053a83f7b6e
Anton Kralin <kralin.dart@gmail.com>
2026-03-24 15:00:15 +0300

    change onecttn

--- apps/onec/models.py
+++ apps/onec/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+from apps.shtrih.models import Models
+
 
 class OneCTTN(models.Model):
     number = models.CharField(max_length=50)
     series = models.CharField(max_length=50, blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        onec = OneCTTN.objects.filter(number=self.number, series=self.series).first()
+        if onec:
+            OneCTTNItem.objects.filter(onec_ttn=onec).delete()
+            return None
+
+        return super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-id']
@@ -16,7 +26,7 @@ class OneCTTN(models.Model):
 
 class OneCTTNItem(models.Model):
     onec_ttn = models.ForeignKey(OneCTTN, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    model = models.ForeignKey(Models, on_delete=models.CASCADE, null=True, blank=True, db_constraint=False)
     count = models.PositiveIntegerField(default=1)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
