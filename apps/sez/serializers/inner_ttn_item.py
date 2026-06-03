5779c18a053dc8135fa8fe965c7602681466fba4
Anton Kralin <kralin.dart@gmail.com>
2026-06-03 15:11:18 +0300

    fix innerttn item patch

--- apps/sez/serializers/inner_ttn_item.py
+++ apps/sez/serializers/inner_ttn_item.py
@@ -12,7 +12,17 @@ class InnerTTNItemsPostSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if data['quantity'] <= 0:
             raise serializers.ValidationError('Количество не может быть меньше 0')
-        if InnerTTNItems.objects.filter(inner_ttn=data['inner_ttn'], model_name=data['model_name']).exists():
+
+        model_name = data.get('model_name')
+        inner_ttn = data.get('inner_ttn')
+        if not model_name:
+            model_name = self.instance.model_name
+        if not inner_ttn:
+            inner_ttn = self.instance.inner_ttn
+        item_id = None
+        if self.instance:
+            item_id = self.instance.id
+        if InnerTTNItems.objects.exclude(id=item_id).filter(inner_ttn=inner_ttn, model_name=model_name).exists():
             raise serializers.ValidationError('Товар в накладной уже существует')
         return data
 
